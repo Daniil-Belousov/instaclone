c4dad094b2a8ae2e784b25e8326dd2a8e6c406cd
Daniil Belousov <d.belousov.exceedteam@gmail.com>
2022-12-20 17:52:44 +0300

    clear project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,7 +11,6 @@ export default function Home() {
         <link rel="icon" href="/fav.svg" />
       </Head>
       <main className={styles.main}>
-        NEEDESIGN
       </main>
     </>
   )
